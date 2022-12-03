d2bd25443d99ec266792f906e68bafe9482494ee
Felipe Santos <felipe.carvalho.bs.contato@gmail.com>
2022-12-03 02:20:58 -0300

    enhancement: remocao de anotacao inutil

--- CommonsUtilities/Observer/src/main/java/io/github/zodh/adapters/observer/Identifiable.java
+++
@@ -1,9 +0,0 @@
-package io.github.zodh.adapters.observer;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface Identifiable {
-
-}
--- CommonsUtilities/Observer/src/main/java/io/github/zodh/adapters/observer/Observer.java
+++ CommonsUtilities/Observer/src/main/java/io/github/zodh/adapters/observer/Observer.java
@@ -1,6 +1,5 @@
 package io.github.zodh.adapters.observer;
 
-import io.github.zodh.evaluation.ObjectEvaluator;
 import org.springframework.context.ApplicationListener;
 
 public abstract class Observer <T extends Observable> implements ApplicationListener<T>, Searchable<T>{
@@ -9,11 +8,7 @@ public abstract class Observer <T extends Observable> implements ApplicationList
     if (identifier == null) {
       throw new RuntimeException("It is not possible to get an object with null identifier");
     }
-    T subject = peek(identifier);
-    if (Boolean.TRUE.equals(ObjectEvaluator.isNotNullSafeEval(subject::getClass)) && subject.getClass().isAnnotationPresent(Identifiable.class)) {
-      throw new RuntimeException("It is not possible to get an object without @Identifiable annotation");
-    }
-    return subject;
+    return peek(identifier);
   }
 
 }
