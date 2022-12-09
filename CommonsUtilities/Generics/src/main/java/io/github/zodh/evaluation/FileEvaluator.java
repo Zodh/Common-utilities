fc3a7433cac4e53307c6c3b2965467f87e5e47a5
felipe.santos <felipe.santos@objective.com.br>
2022-12-09 11:51:26 -0300

    feat: file evaluator to peek file data

---
+++ CommonsUtilities/Generics/src/main/java/io/github/zodh/evaluation/FileEvaluator.java
@@ -0,0 +1,15 @@
+package io.github.zodh.evaluation;
+
+import java.io.File;
+
+public class FileEvaluator {
+
+    public static String getFileInformation(final File file) {
+        String sb = "The file " + file.getName() + " is not valid. \n" +
+                "Path: " + file.getAbsolutePath() + " (absolute path) \n" +
+                "Exists? " + file.exists() + "\n" +
+                "Read: " + file.canRead() + " - Write: " + file.canWrite() + " - Execute: " + file.canExecute();
+        return sb;
+    }
+
+}
